b34805f16d9e9f54c1b6b6b4105641a5929e78f4
hornsilk <hornsilk01@gmail.com>
2022-11-25 12:01:01 -0700

    support hardcoded West_Gash and Sheogorad regions

--- regions.py
+++ regions.py
@@ -2,6 +2,76 @@ def check_region(cellX, cellY):
     x = cellX
     y = cellY
 
+    ## Vanilla
+    # West_Gash
+    if x == -17 and y in range(11,16 +1):
+        return 'West_Gash'
+    if x == -16 and y in range(10,17 +1):
+        return 'West_Gash'
+    if x == -15 and y in range(8,17 +1):
+        return 'West_Gash'
+    if x == -14 and y in range(8,17 +1):
+        return 'West_Gash'
+    if x == -13 and y in range(6,18 +1):
+        return 'West_Gash'
+    if x == -12 and y in range(6,20 +1):
+        return 'West_Gash'
+    if x == -11 and y in range(6,20 +1):
+        return 'West_Gash'
+    if x == -10 and y in range(6,20 +1):
+        return 'West_Gash'
+    if x == -9 and y in range(5,20 +1):
+        return 'West_Gash'
+    if x == -8 and y in range(5,17 +1):
+        return 'West_Gash'
+    if x == -7 and y in range(5,17 +1):
+        return 'West_Gash'
+    if x == -6 and y in range(4,14 +1):
+        return 'West_Gash'
+    if x == -5 and y in range(4,7 +1):
+        return 'West_Gash'
+    if x == -4 and y in range(0,5 +1):
+        return 'West_Gash'
+    if x == -4 and y in range(-3,-2 +1):
+        return 'West_Gash'
+    if x == -3 and y in range(-3,4 +1):
+        return 'West_Gash'
+    if x == -2 and y in range(-3,4 +1):
+        return 'West_Gash'
+    if x == -2 and y in range(-3,3 +1):
+        return 'West_Gash'
+    # Sheogorad
+    if x == -8 and y in range(18,22 +1):
+        return 'Sheogorad'
+    if x == -7 and y in range(18,24 +1):
+        return 'Sheogorad'
+    if x == -6 and y in range(18,25 +1):
+        return 'Sheogorad'
+    if x == -5 and y in range(18,27 +1):
+        return 'Sheogorad'
+    if x in range(-4,0 +1) and y in range(19,27 +1):
+        return 'Sheogorad'
+    if x == 1 and y in range(18,27 +1):
+        return 'Sheogorad'
+    if x in range(2,8 +1) and y in range(17,26 +1):
+        return 'Sheogorad'
+    if x in range(5,7 +1) and y == 16:
+        return 'Sheogorad'
+    if x == 9 and y in range(20,26 +1):
+        return 'Sheogorad'
+    if x == 10 and y in range(23,24 +1):
+        return 'Sheogorad'
+    if x == 11 and y == 23:
+        return 'Sheogorad'
+    # Ashlands
+    # Red_Mountain
+    # Bitter_Coast
+    # Ascadian_Isles
+    # Grazelands
+    # Azuras_Coast
+    # Molag_Amur
+
+
     ## Solstheim
     # Moespring_Mountains
     if x == -21 and y == 29:
@@ -91,14 +161,17 @@ def check_region(cellX, cellY):
 def isReleased(region_name, mod_list):
     game_regions = []
 
-    if 'Tribunal' in mod_list:
-        game_regions.append('Mournhold')
+    # Add vanilla regions
+    game_regions += ['West_Gash','Sheogorad','Ashlands','Red_Mountain','Bitter_Coast','Ascadian_Isles','Grazelands','Azuras_Coast','Molag_Amur']
 
-    
+
+    ## Add expansion regions
     if 'Anthology_Solstheim' in mod_list:
         game_regions += ['Moespring_Mountains','Felsaad_Coast','Isinfier_Plains','Hirstaang_Forest']
     # elif 'Bloodmoon' in mod_list:
     #     game_regions.append('Vanilla_Solstheim')
+    # if 'Tribunal' in mod_list:
+    #     game_regions.append('Mournhold')
 
 
 
