83593e0ed6856c1e8f1eda3d1232e5c811e67c3a
hornsilk <hornsilk01@gmail.com>
2022-11-25 14:45:35 -0700

    support current TR_Mainland

--- regions.py
+++ regions.py
@@ -152,9 +152,11 @@ def check_region(cellX, cellY):
     if x == 11 and y in range(24,29 +1): return 'Sea_of_Ghosts_E'
     if x == 12 and y in range(23,29 +1): return 'Sea_of_Ghosts_E'
     if x in range(13,18 +1) and y in range(21,29 +1): return 'Sea_of_Ghosts_E'
-    if x == 19 and y in range(23,32 +1): return 'Sea_of_Ghosts_E'
-    if x == 20 and y in range(23,33 +1): return 'Sea_of_Ghosts_E'
-    if x in range(21,22 +1) and y in range(29,33 +1): return 'Sea_of_Ghosts_E'
+    if x == 19 and y in range(23,29 +1): return 'Sea_of_Ghosts_E'
+    # if x == 19 and y in range(30,32 +1): return 'Sea_of_Ghosts_E'
+    if x == 20 and y in range(23,29 +1): return 'Sea_of_Ghosts_E'
+    # if x == 20 and y in range(30,33 +1): return 'Sea_of_Ghosts_E'
+    # if x in range(21,22 +1) and y in range(29,33 +1): return 'Sea_of_Ghosts_E'
     if x == 21 and y in range(23,24 +1): return 'Sea_of_Ghosts_E'
     if x == 22 and y in range(22,24 +1): return 'Sea_of_Ghosts_E'
     if x == 23 and y in range(21,24 +1): return 'Sea_of_Ghosts_E'
@@ -190,12 +192,13 @@ def check_region(cellX, cellY):
     # Telvanni_Isles (must be after Molagreahd and Sea_of_Ghosts_E)
     if x in range(30,40 +1) and y in range(14,22 +1): return 'Telvanni_Isles'
     if x in range(41,46 +1) and y in range(12,22 +1): return 'Telvanni_Isles'
-    if x in range(47,49 +1) and y in range(13,22 +1): return 'Telvanni_Isles'
+    if x in range(47,48 +1) and y in range(13,22 +1): return 'Telvanni_Isles'
+    # if x == 49 and y in range(13,22 +1): return 'Telvanni_Isles'
     if x in range(42,44 +1) and y in range(10,11 +1): return 'Telvanni_Isles'
     if x == 45 and y == 11: return 'Telvanni_Isles'
-    if x in range(53,56 +1) and y in range(13,17 +1): return 'Telvanni_Isles'
-    if x == 57 and y in range(14,16 +1): return 'Telvanni_Isles'
-    if x in range(54,56 +1) and y == 18: return 'Telvanni_Isles'
+    # if x in range(53,56 +1) and y in range(13,17 +1): return 'Telvanni_Isles'
+    # if x == 57 and y in range(14,16 +1): return 'Telvanni_Isles'
+    # if x in range(54,56 +1) and y == 18: return 'Telvanni_Isles'
     # Dagon_Urul
     if x in range(13,26 +1) and y in range(6,20 +1): return 'Dagon_Urul'
     if x == 22 and y in range(4,5 +1): return 'Dagon_Urul'
@@ -267,6 +270,7 @@ def check_region(cellX, cellY):
     # Alt_Orethan
     if x in range(27,28 +1) and y in range(-28,-22 +1): return 'Alt_Orethan'
     if x in range(19,26 +1) and y in range(-23,-22 +1): return 'Alt_Orethan'
+    if x ==14 and y == -32: return 'Alt_Orethan'
     # Sundered_Scar
     if x in range(14,18 +1) and y in range(-23,-22 +1): return 'Sundered_Scar'
     if x ==13 and y == -22: return 'Sundered_Scar'
@@ -276,9 +280,33 @@ def check_region(cellX, cellY):
     if x ==10 and y in range(-18,-16 +1): return 'Sundered_Scar'
     if x ==9 and y in range(-17,-16 +1): return 'Sundered_Scar'
     # Aanthirin
+    if x in range(7,13 +1) and y == -34: return 'Aanthirin'
+    if x in range(6,13 +1) and y in range(-33,-32 +1): return 'Aanthirin'
+    if x in range(0,13 +1) and y in range(-31,-23 +1): return 'Aanthirin'
+    if x in range(1,12 +1) and y in range(-22,-20 +1): return 'Aanthirin'
+    if x in range(3,11 +1) and y in range(-19,-16 +1): return 'Aanthirin'
+    if x == 2 and y in range(-17,-16 +1): return 'Aanthirin'
     # Armun_Ashlands
+    if x in range(-9,-6 +1) and y ==-31: return 'Armun_Ashlands'
+    if x in range(-10,-1 +1) and y in range(-30,-23 +1): return 'Armun_Ashlands'
+    if x in range(-10,-3 +1) and y == -22: return 'Armun_Ashlands'
+    if x in range(-13,-11 +1) and y in range(-28,-23 +1): return 'Armun_Ashlands'
+    if x in range(-14,-13 +1) and y in range(-27,-22 +1): return 'Armun_Ashlands'
+    if x == -15 and y in range(-25,-21 +1): return 'Armun_Ashlands'
     # Roth_Roryn
+    if x == -15 and y in range(-20,-18 +1): return 'Roth_Roryn'
+    if x in range(-14,2 +1) and y in range(-22,-15 +1): return 'Roth_Roryn'
+    if x in range(-8,-1 +1) and y in range(-14,-9 +1): return 'Roth_Roryn'
+    if x ==-9 and y in range(-11,-9 +1): return 'Roth_Roryn'
     # Velothi_Mountains_E
+    if x in range(-16,-15 +1) and y in range(-16,-15 +1): return 'Velothi_Mountains_E'
+    if x in range(-18,-15 +1) and y == -17: return 'Velothi_Mountains_E'
+    if x in range(-18,-16 +1) and y in range(-26,-18 +1): return 'Velothi_Mountains_E'
+    if x == -19 and y in range(-26,-20 +1): return 'Velothi_Mountains_E'
+    if x == -20 and y in range(-26,-21 +1): return 'Velothi_Mountains_E'
+    if x == -21 and y in range(-25,-22 +1): return 'Velothi_Mountains_E'
+    if x == -15 and y == -26: return 'Velothi_Mountains_E'
+
 
     return 'unknown'
 
@@ -286,7 +314,7 @@ def check_region(cellX, cellY):
 def isReleased(region_name, mod_list):
     game_regions = []
 
-    # Add vanilla regions
+    ## Add vanilla regions
     game_regions += ['West_Gash','Sheogorad','Ashlands','Red_Mountain','Bitter_Coast','Ascadian_Isles','Grazelands','Azuras_Coast','Molag_Amur']
 
     ## Add expansion regions
@@ -303,7 +331,6 @@ def isReleased(region_name, mod_list):
 
     if 'TR_Mainland' in mod_list:
         game_regions += tr_released_regions
-
 
 
     if region_name in game_regions:
